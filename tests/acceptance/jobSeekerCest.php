77a36eb7a9040489d3d608ac2639359f5dbaba1a
jarond <jaron@biostatistika.cz>
2020-07-28 17:57:31 +0200

    update - Jul 2020

--- tests/acceptance/jobSeekerCest.php
+++ tests/acceptance/jobSeekerCest.php
@@ -7,13 +7,18 @@ class jobSeekerCest{
         $I->amOnUrl('https://google.com');
         $I->fillField(['css' => 'input.gLFyf'], 'MoroSystems');//main input field
         $this->wsClick($I, ['css' => 'input.gNO89b']);//search button
-        for ($i=1; $i <= 5; $i++) {
-            $this->wsClick($I, ['css' => '.srg > .g:nth-child('.$i.') h3']);//$ith of the results (first two are ommited)
+        for ($i=1; $i <= 8; $i++) {
+            try {
+                $this->wsClick($I, ['css' => '#rso > div.g:nth-child('.$i.') div.r > a']);//$ith of the results (first two are ommited)
+            } catch (Exception $e) {
+                continue;
+            }
+
             try {
                 $I->see('morosystems');//check it is a moro webpage
-                $I->see('references');//check it is an english webpage
-                $I->click(['css' => '#menu-secondary-menu > li.headerLanguage > a > img']);//switch to czech version
-                $this->wsClick($I, ['css'=>'.js-careerLink']);//click to career
+                $I->see('reference');
+                $this->wsClick($I, ['css' => '#menu-hlavni-menu span.btn__text']);//contact page
+                $I->see('MoroSystems, s.r.o.');
             } catch (Exception $e) {
                 $I->moveBack();//if page is wrong, go back to results
                 continue;
@@ -21,12 +26,12 @@ class jobSeekerCest{
             break;
         }
         $I->comment("\e[1mTest finished press enter to close window.\e[0m");
-        $I->pause();//
+        $I->pause();
     }
 
 
     private function wsClick(AcceptanceTester $I, $selector){
-        $I->waitForElementClickable($selector);
+        $I->waitForElementClickable($selector, 3);
         $I->scrollTo($selector,0,-100);
         $I->click($selector);
     }
